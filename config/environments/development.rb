07a2e4dd679b59a633bd35a2a6a1a1b77e009e46
Chitroopa Manikkavasagam and Jonathon Kang <chitroopa+jonathon@>
2017-07-06 15:17:38 -0700

    updated environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   #
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://damp-reaches-91084.herokuapp.com' }
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
